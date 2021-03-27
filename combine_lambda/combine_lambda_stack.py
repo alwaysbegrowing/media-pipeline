4aa988e927760601d6725c0d4ce6424c692f15e4
Chandler Lofland <chandler@chand1012.dev>
2021-03-27 10:26:56 -0400

    Add initial files

--- combine_lambda/combine_lambda_stack.py
+++ combine_lambda/combine_lambda_stack.py
@@ -1,11 +1,8 @@
 from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+from aws_cdk import aws_sqs as sqs
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_mediaconvert as mediaconvert
+from aws_cdk.aws_lambda_event_sources import SqsEventSource
 
 class CombineLambdaStack(cdk.Stack):
 
@@ -13,3 +10,14 @@ class CombineLambdaStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        queue = sqs.Queue(self, "ClipInputQueue")
+
+        event_source = SqsEventSource(queue=queue, batch_size=1, enabled=True)
+
+        sqs_lambda = _lambda.Function(self, 'ClipInputLambda',
+            handler='lambda.handler',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.asset('lambda')
+        )
+
+        sqs_lambda.add_event_source(event_source)
---
+++ lambda/handler.py
@@ -0,0 +1,6 @@
+import json
+import boto3
+
+def handler(event, context):
+    print(event)
+    print(context)

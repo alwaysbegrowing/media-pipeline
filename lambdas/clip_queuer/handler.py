81ae5879ff6c5e371ca282214e2af1d255bb1666
Russell <russell@deephire.com>
2021-03-28 17:12:27 -0500

    add working example of using mediaconvert to combine clips

--- lambdas/clip_queuer/handler.py
+++ lambdas/clip_queuer/handler.py
@@ -1,18 +1,96 @@
-import json
 import boto3
+import json
 # import ffmpy # how the heck do i import this!!
+# import youtube_dl
+
+# ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+def makeInput(file):
+    return {
+        "AudioSelectors": {
+            "Audio Selector 1": {
+                "Offset": 0,
+                "DefaultSelection": "DEFAULT",
+                "ProgramSelection": 1
+            }
+        },
+        "VideoSelector": {
+            "ColorSpace": "FOLLOW",
+            "Rotate": "DEGREE_0",
+            "AlphaBehavior": "DISCARD"
+        },
+        "FilterEnable": "AUTO",
+        "PsiControl": "USE_PSI",
+        "FilterStrength": 0,
+        "DeblockFilter": "DISABLED",
+        "DenoiseFilter": "DISABLED",
+        "InputScanType": "AUTO",
+        "TimecodeSource": "ZEROBASED",
+        "FileInput": file
+    }
+
+
+mediaconvert_endpoint = 'https://lxlxpswfb.mediaconvert.us-east-1.amazonaws.com'
+
+
+def buildObj(s3Urls):
+    with open("job.json", "r") as jsonfile:
+        job_object = json.load(jsonfile)
+
+
+
+    for url in s3Urls:
+        job_object['Settings']['Inputs'].append(makeInput(url))
+    return job_object
+
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 
 def handler(event, context):
+
+    mediaconvert_client = boto3.client(
+        'mediaconvert', endpoint_url=mediaconvert_endpoint)
 
     # get messages from API
     # download messages with timestamp
     # upload messages to s3 bucket
-    # tell mediaconvert to combine clips
-   
+    # add s3 urls to job object
+
+    # upload_file(file_name)
+
+    s3Urls = ['s3://pillarclips/clip_11_im_baguette_2021-02-14T04:58:48Z.mp4',
+              's3://pillarclips/clip_11_im_baguette_2021-02-14T04:58:48Z.mp4']
+    job_object = buildObj(s3Urls)
+
+    convertResponse = mediaconvert_client.create_job(**job_object)
+    print(convertResponse)
     return {
-    "statusCode": 200,
-    "headers": {
-        "Content-Type": "application/json"
-    },
-    "body": event['body']
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": event['body']
     }
+
+
--- stack/stack.py
+++ stack/stack.py
@@ -5,6 +5,7 @@ from aws_cdk.aws_lambda_event_sources import SqsEventSource
 from aws_cdk import (core as cdk,
                      aws_apigateway as apigateway,
                      aws_sqs as sqs,
+                     aws_s3 as s3,
                      aws_lambda as lambda_,
                      aws_mediaconvert as mediaconvert)
 
@@ -14,43 +15,53 @@ class RenderLambdaStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        individual_clips = s3.Bucket(self,
+                                     "IndividualClips")
+
+        combined_clips = s3.Bucket(self,
+                                   "CombinedClips")
+                                   
+
         mediaconvert_queue = mediaconvert.CfnQueue(self, id="ClipCombiner")
+        individual_clips.grant_read(mediaconvert_queue)
+        combined_clips.grant_write(mediaconvert_queue)
 
 
         clip_api = apigateway.RestApi(self, "clip-api",
-                                 rest_api_name="Clips service",
-                                 description="Service handles combining clips")
+                                      rest_api_name="Clips service",
+                                      description="Service handles combining clips")
 
         clip_queuer = lambda_.Function(self, 'ClipQueuer',
-                                      handler='handler.handler',
-                                      runtime=lambda_.Runtime.PYTHON_3_8,
-                                      code=lambda_.Code.from_asset(
-                                          path=os.path.join('lambdas/clip_queuer')),
-                                      environment={
-                                          'MEDIA_QUEUE': mediaconvert_queue.attr_arn}
-                                      )
+                                       handler='handler.handler',
+                                       runtime=lambda_.Runtime.PYTHON_3_8,
+                                       code=lambda_.Code.from_asset(
+                                           path=os.path.join('lambdas/clip_queuer')),
+                                       environment={
+                                           'MEDIA_QUEUE': mediaconvert_queue.attr_arn}
+                                       )
 
+
+        individual_clips.grant_read(clip_queuer)
+        
         getfromQueue = apigateway.LambdaIntegration(clip_queuer,
-                request_templates={"application/json": '{ "statusCode": "200" }'})
+                                                    request_templates={"application/json": '{ "statusCode": "200" }'})
 
         clips_endpoint = clip_api.root.add_resource("clips")
 
         clips_endpoint.add_method("POST", getfromQueue)
         clips_endpoint.add_method("GET", getfromQueue)
 
+        # queue = sqs.Queue(self, "ClipInputQueue")
 
-        queue = sqs.Queue(self, "ClipInputQueue")
+        # event_source = SqsEventSource(queue=queue, batch_size=1, enabled=True)
 
-        event_source = SqsEventSource(queue=queue, batch_size=1, enabled=True)
+        # sqs_lambda = lambda_.Function(self, 'ClipCreator',
+        #                               handler='handler.handler',
+        #                               runtime=lambda_.Runtime.PYTHON_3_8,
+        #                               code=lambda_.Code.from_asset(
+        #                                   path=os.path.join('lambdas/clip_creator')),
+        #                               environment={
+        #                                   'MEDIA_QUEUE': mediaconvert_queue.attr_arn}
+        #                               )
 
-
-        sqs_lambda = lambda_.Function(self, 'ClipCreator',
-                                      handler='handler.handler',
-                                      runtime=lambda_.Runtime.PYTHON_3_8,
-                                      code=lambda_.Code.from_asset(
-                                          path=os.path.join('lambdas/clip_creator')),
-                                      environment={
-                                          'MEDIA_QUEUE': mediaconvert_queue.attr_arn}
-                                      )
-
-        sqs_lambda.add_event_source(event_source)
+        # sqs_lambda.add_event_source(event_source)

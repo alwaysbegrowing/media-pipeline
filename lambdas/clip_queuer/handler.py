087f5bd2c6dd4fce2bbf828ce51ac7656674469b
chand1012 <chandler@chand1012.dev>
2021-03-30 23:07:19 -0400

    Update http api for step functions

--- lambdas/clip_queuer/handler.py
+++ lambdas/clip_queuer/handler.py
@@ -1,8 +1,8 @@
-import os
 import json
+import os
+import uuid
 
 import boto3
-
 import streamlink
 
 
@@ -13,6 +13,9 @@ def handler(event, context):
         'clips': [{'start_time': 55, 'end_time': 90, 'name': 'clip12'}],
         'original_url': 'https://www.twitch.tv/videos/964350897',
     }
+
+    The response body will be the state input body with the response
+    from the request to create the state appended to the state input
     '''
     job = json.loads(event.get('body'))
 
@@ -22,7 +25,12 @@ def handler(event, context):
     streams = streamlink.streams(original_url)
     best_stream = streams.get('best').url
 
-    sqs = boto3.client('sqs')
+    state = {
+        'stream_manifest_url': best_stream,
+        'clips': []
+    }
+
+    sfn = boto3.client('stepfunctions')
 
     for clip in clips: # this will be changed to add tasks
         data = {
@@ -32,17 +40,21 @@ def handler(event, context):
             'name': clip.get('name') + '.mp4',
             'bucket': os.getenv('BUCKET')
         }
-        data_str = json.dumps(data)
-        resp = client.send_message(
-            QueueUrl=os.getenv('DOWNLOAD_QUEUE'),
-            MessageBody=data_str
-        )
+        state['clips'].append(data)
+
+    resp = sfn.start_execution(
+        stateMachineArn=os.getenv('STEPFUNCTION_ARN'),
+        name=uuid.uuid4(),
+        input=json.dumps(state)
+    )
+
+    state.update(resp)
 
     return {
         'statusCode': 200
         'headers': {
             'Content-Type': 'application/json'
         },
-        body: '{}'
+        body: json.dumps(state)
     }
         

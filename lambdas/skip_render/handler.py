b5669bf029d7b9401a0f3ab033ec1501b02d1618
chand1012 <chandler@chand1012.dev>
2021-04-13 13:47:37 -0400

    Finish render skip lambda

--- lambdas/skip_render/handler.py
+++ lambdas/skip_render/handler.py
@@ -1,28 +1,50 @@
 import json
 import os
 
+import boto3
+
 
 def handler(event, context):
     '''
     Will take a list of clips and make a notification event out of them.
     '''
 
-    os.getenv('BUCKET_DNS')
+    BUCKET_DNS = os.getenv('BUCKET_DNS')
 
     clips = []
     for item in event:
         clip = item.get('Payload')
         if not clip is None:
             name = clip.get('name')
-            clip['url'] = f'https://{BUCKET_DNS}/{name}'
-            clips.append(clip)
+            position = clip.get('position')
+            new_clip = {
+                'url': f'https://{BUCKET_DNS}/{name}',
+                'name': name,
+                'position': position
+            }
+            clips.append(new_clip)
 
     sorted(clips, key=lambda clip: clip['position'])
 
+    print(clips)
+
     body = {
         'clips': clips,
-        'render': False
+        'render': False,
+        'video': None
     }
+
+    payload = json.dumps(body).encode()
+
+    lambda_ = boto3.client('lambda')
+    LAMBDA_ARN = os.getenv('LAMBDA_ARN')
+    resp = lambda_.invoke(
+        FunctionName=LAMBDA_ARN,
+        Payload=payload,
+        InvocationType='Event'
+    )
+
+    body.update(resp)
 
     return {
         "statusCode": 200,

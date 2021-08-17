f8f59494e3ebf7206825535ded85900fcd9427f3
Russell <russell@deephire.com>
2021-08-16 19:40:11 -0500

    handle failure event

--- lambdas/transcoding_progress/handler.py
+++ lambdas/transcoding_progress/handler.py
@@ -8,10 +8,12 @@ def handler(event, context):
     token = f'{user_metadata["TaskToken1"]}{user_metadata["TaskToken2"]}{user_metadata["TaskToken3"]}'
 
     if (event['detail']['status'] == "COMPLETE"):
-        stepfunctions.send_task_success(taskToken=token, output=json.dumps({'status': 'complete'}))
-    
+        stepfunctions.send_task_success(
+            taskToken=token, output=json.dumps({'status': 'complete'}))
+
     if (event['detail']['status'] == "STATUS_UPDATE"):
         stepfunctions.send_task_heartbeat(taskToken=token)
-    
+
     if (event['detail']['status'] == "ERROR"):
-        stepfunctions.send_task_failure(taskToken=token, error=event['detail']['errorMessage'])
+        stepfunctions.send_task_failure(
+            taskToken=token, error=event['detail']['errorCode'], cause=event['detail']['errorMessage'])

5ef6baadec536542670b6420f83120fdac7d21a3
Russell <russell@deephire.com>
2021-08-19 19:56:09 -0500

    simplify flow

--- lambdas/notify/handler.py
+++ lambdas/notify/handler.py
@@ -5,15 +5,35 @@ import boto3
 FROM_EMAIL = os.getenv('FROM_EMAIL')
 email_client = boto3.client('ses')
 
+
+def nth_repl(s, sub, repl, n):
+    find = s.find(sub)
+    # If find is not -1 we have found at least one match for the substring
+    i = find != -1
+    # loop util we find the nth or we find no match
+    while find != -1 and i != n:
+        # find + 1 means we start searching from after the last match
+        find = s.find(sub, find + 1)
+        i += 1
+    # If i is equal to n we found nth match so replace
+    if i == n:
+        return s[:find] + repl + s[find+len(sub):]
+    return s
+
+# formats s3 url to a http url
+def s3_to_http(s3_url):
+    return nth_repl(s3_url, '/', '.s3.amazonaws.com/', 3).replace('s3://', 'https://')
+
+
 def handler(event, context):
-    bucket = event['mediaConvertResult']['Bucket']
-    Key = event['mediaConvertResult']['Key']
+    s3_url = event['mediaConvertResult']['outputFilePath']
     request_email = event['user']['email']
     display_name = event['user']['display_name']
 
 
-    compilation_file_url = 'https://' + bucket + '.s3.amazonaws.com' + '/' + Key
-   
+    compilation_file_url = s3_to_http(s3_url)
+
+
 
     html = f'''
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">

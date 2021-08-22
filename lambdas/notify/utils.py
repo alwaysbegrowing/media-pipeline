7486ae6a9016c8e644f10a6f0b2c39e8ed2ee2a0
Russell <russell@deephire.com>
2021-08-22 13:38:19 -0500

    add better explanation of confusing util functin

--- lambdas/notify/utils.py
+++ lambdas/notify/utils.py
@@ -1,17 +1,28 @@
-def nth_repl(s, sub, repl, n):
+
+
+def replace_nth_occurance(s, sub, repl, n):
+    '''
+    # https://stackoverflow.com/questions/35091557/replace-nth-occurrence-of-substring-in-string
+    # In [14]: s = "foobarfoofoobarbar"
+
+    # In [15]: replace_nth_occurance(s, "bar","replaced",3)
+    # Out[15]: 'foobarfoofoobarreplaced'
+
+    # In [16]: replace_nth_occurance(s, "foo","replaced",3)
+    # Out[16]: 'foobarfooreplacedbarbar'
+
+    # In [17]: replace_nth_occurance(s, "foo","replaced",5)
+    # Out[17]: 'foobarfoofoobarbar'
+    '''
     find = s.find(sub)
-    # If find is not -1 we have found at least one match for the substring
     i = find != -1
-    # loop util we find the nth or we find no match
     while find != -1 and i != n:
-        # find + 1 means we start searching from after the last match
         find = s.find(sub, find + 1)
         i += 1
-    # If i is equal to n we found nth match so replace
     if i == n:
         return s[:find] + repl + s[find+len(sub):]
     return s
 
 # formats s3 url to a http url
 def s3_to_http(s3_url):
-    return nth_repl(s3_url, '/', '.s3.amazonaws.com/', 3).replace('s3://', 'https://')
+    return replace_nth_occurance(s3_url, '/', '.s3.amazonaws.com/', 3).replace('s3://', 'https://')
